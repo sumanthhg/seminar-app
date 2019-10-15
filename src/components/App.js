ea43c8d74c22c7839d2475fc76906516b17702ac
sumanth h g <sumanth.hgbhat@gmail.com>
2019-10-15 08:53:58 -0700

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Container from '@material-ui/core/Container';
+
+const App = () => {
+    return (
+        <Container maxWidth="sm">App</Container>
+    )
+};
+export default App;
